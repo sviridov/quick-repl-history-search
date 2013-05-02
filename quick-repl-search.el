e08f8968299f363884d8d4505dd6a00ca9fb0efa
Alexander Sviridov <sviridov.vmi@gmail.com>
2013-05-02 22:02:45 +0600

    In progress..

--- quick-repl-search.el
+++ quick-repl-search.el
@@ -23,14 +23,18 @@
     (define-key map (kbd "C-r") 'quick-repl-search-previous)
     (define-key map (kbd "C-g") 'quick-repl-search-abort)
     map)
-  "Keymap for the QUICK-REPL-SEARCH prompt buffers"
+  "Keymap for the quick-repl-search prompt buffers"
   :group 'quick-repl-search)
 
 (defvar quick-repl-search--modes-table (make-hash-table)
-  "TODO")
+  "Variable in which stored different information about modes.
+   Information represented as a plist.
+   Plist keys:
+    `:get-history-function'
+    `:kill-input-function'")
 
 (defvar quick-repl-search-mode nil
-  "Minor mode for QUICK-REPL-SEARCH prompt buffer")
+  "Minor mode for quick-repl-search prompt buffer")
 
 (make-variable-buffer-local 'quick-repl-search-mode)
 
@@ -38,14 +42,21 @@
 (add-to-list 'minor-mode-map-alist `(quick-repl-search-mode . ,quick-repl-search-mode-map))
 
 (defvar quick-repl-search--mode-line-format
-  '(" *quick-repl-search*"))
+  '(" *quick-repl-search*")) ;; TODO: Add more information
 
 ;;;=================================================================================================
 
-(defmacro quick-repl-search-add-mode (mode history-variable &key (key #'identity)
-                                                                    kill-input-function
-                                                                    mode-map)
-  "TODO")
+(cl-defmacro quick-repl-search-add-mode (majors-mode history-variable &key (key #'identity)
+                                                                           kill-input-function
+                                                                           mode-map
+                                                                           (mode-map-key (kbd "C-r")))
+ `(progn
+    (setf (gethash ',majors-mode quick-repl-search--modes-table)
+          (list
+           :get-history-function (lambda () (funcall #',key ',history-variable))
+           :kill-input-function ,kill-input-function))
+
+    (define-key ,mode-map ,mode-map-key 'quick-repl-search)))
 
 ;;;=================================================================================================
 
@@ -54,6 +65,13 @@
   (select-window (split-window-vertically -4))
   (switch-to-buffer (generate-new-buffer "*quick-repl-search*"))
   (yank))
+
+;;;=================================================================================================
+
+(eval-after-load "slime"
+ `(quick-repl-search-add-mode slime-repl-mode slime-repl-input-history
+                              :kill-input-function #'slime-repl-kill-input
+                              :mode-map slime-repl-mode-map))
 
 ;;;=================================================================================================
 
