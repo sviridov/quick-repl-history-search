4b3f60bd1aa093e4f2715444b9c2263bd96790ea
Alexander Sviridov <sviridov.vmi@gmail.com>
2013-05-02 18:09:25 +0600

    Initial commit

---
+++ quick-history-search.el
@@ -0,0 +1,20 @@
+;;;; quick-history-search.el
+;;;;
+;;;; quick-history-search is available under the MIT license;
+;;;; see LICENSE for details
+;;;;
+;;;; For a detailed introduction see: README.md
+;;;;
+;;;; Copyright (C) 2013 Sviridov Alexander <sviridov.vmi@gmail.com>
+
+(defgroup quick-history-search nil
+  "TODO"
+  :group 'emacs
+  :version "1.0"
+  :link '(emacs-library-link :tag "Lisp File" "quick-history-search.el"))
+
+;;;=================================================================================================
+
+
+
+;;;=================================================================================================
